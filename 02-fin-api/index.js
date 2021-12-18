f7bc40fb8a18cc066855a46f1713cbae83a53563
Juliano Vendramini Codognos <66084938+JulianoVendramini@users.noreply.github.com>
2021-12-18 11:53:37 -0300

    feat: verifed if the customer already have an account

--- 02-fin-api/index.js
+++ 02-fin-api/index.js
@@ -8,10 +8,15 @@ const customers = [];
 
 app.post('/account', (req, res) => {
     const { cpf, name } = req.body;
-    const id = uuidv4();
-
+    
+    const customerAlreadyExists = customers.some(customer => customer.cpf === cpf);
+    
+    if(customerAlreadyExists){
+        res.status(400).json({ error: 'Customer already exists' });
+    }
+   
     customers.push({
-        id,
+        id: uuidv4(),
         cpf,
         name,
         statement: []
