33972d3cff7bc34319e24051762baefe129c3f4b
Juliano Vendramini Codognos <66084938+JulianoVendramini@users.noreply.github.com>
2021-12-18 01:04:35 -0300

    feat: tested all the HTTP methods

--- 01-fundamentos-nodejs/src/index.js
+++ 01-fundamentos-nodejs/src/index.js
@@ -1,9 +1,33 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    return res.json({message: 'Hello nodemom'});
+app.get('/courses', (req, res) => {
+    const query = req.query;
+    console.log(query);
+    return res.json(['Curso 1', 'Curso 2', 'Curso 3']);
 })
+
+app.post('/courses', (req, res) => {
+    const body = req.body;
+    console.log(body);
+    return res.json(['Curso 1', 'Curso 2', 'Curso 3', 'Curso 4']);
+})
+
+app.put('/courses/:id', (req, res) => {
+    const { id } = req.params;
+    console.log(id)
+    return res.json(['Curso 6', 'Curso 2', 'Curso 3', 'Curso 4']);
+})
+
+app.patch('/courses/:id', (req, res) => {
+    return res.json(['Curso 1', 'Curso 6', 'Curso 3', 'Curso 4']);
+})
+
+app.delete('/courses/:id', (req, res) => {
+    return res.json(['Curso 1', 'Curso 3', 'Curso 4']);
+})
+
 
 app.listen(3333);
