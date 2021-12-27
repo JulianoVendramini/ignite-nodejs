bd6c45f379a3ed4bb9e6dd90bc2e9ca414bbff4e
Juliano Vendramini Codognos <66084938+JulianoVendramini@users.noreply.github.com>
2021-12-27 16:17:21 -0300

    feat: create categories route

--- 04-rentalx/src/server.ts
+++ 04-rentalx/src/server.ts
@@ -1,16 +1,11 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
+
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    return res.json({ message: "Hello teste" });
-});
-
-app.post("/courses", (req, res) => {
-    const { name } = req.body;
-    return res.json({ message: `Hello ${name}` });
-});
+app.use(categoriesRoutes);
 
 app.listen(3333, () => console.log("Server started on port 3333"));
