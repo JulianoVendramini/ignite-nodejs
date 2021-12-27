322d614de677a9311b4d754598ac8a8b45b63225
Juliano Vendramini Codognos <66084938+JulianoVendramini@users.noreply.github.com>
2021-12-26 23:02:00 -0300

    feat: setted prettier/eslint and ts-node-dev

---
+++ 04-rentalx/src/server.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    return res.json({ message: "Hello teste" });
+});
+
+app.listen(3333, () => console.log("Server started on port 3333"));
