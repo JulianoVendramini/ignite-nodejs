2f002ce52c295f4b779bd3f420e94618ac154516
Juliano Vendramini Codognos <66084938+JulianoVendramini@users.noreply.github.com>
2021-12-27 09:31:00 -0300

    feat: debbuger

--- 04-rentalx/src/server.ts
+++ 04-rentalx/src/server.ts
@@ -2,8 +2,15 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
     return res.json({ message: "Hello teste" });
+});
+
+app.post("/courses", (req, res) => {
+    const { name } = req.body;
+    return res.json({ message: `Hello ${name}` });
 });
 
 app.listen(3333, () => console.log("Server started on port 3333"));
