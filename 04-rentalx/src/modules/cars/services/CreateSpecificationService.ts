d7f88803cc88b45118231367ffa5f731764e9a41
Juliano Vendramini Codognos <66084938+JulianoVendramini@users.noreply.github.com>
2022-01-13 19:01:33 -0300

    feat: create specification

--- 04-rentalx/src/modules/cars/services/CreateSpecificationService.ts
+++ 04-rentalx/src/modules/cars/services/CreateSpecificationService.ts
@@ -1,7 +1,23 @@
-class CreateSpacificationService {
-    execute() {
-        console.log("create specification");
+import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
+
+interface IRequest {
+    name: string;
+    description: string;
+}
+
+class CreateSpecificationService {
+    constructor(private specificationsRepository: ISpecificationsRepository) {}
+
+    execute({ name, description }: IRequest): void {
+        const specificationAlreadyExists =
+            this.specificationsRepository.findByName(name);
+
+        if (specificationAlreadyExists) {
+            throw new Error("Specification already exists");
+        }
+
+        this.specificationsRepository.create({ name, description });
     }
 }
 
-export { CreateSpacificationService };
+export { CreateSpecificationService };
