c026b74a2e37fdc15a40eaabfe80e1378457e1b5
Juliano Vendramini Codognos <66084938+JulianoVendramini@users.noreply.github.com>
2022-01-24 18:11:07 -0300

    feat: instance

--- 04-rentalx/src/modules/cars/useCases/listCategories/index.ts
+++ 04-rentalx/src/modules/cars/useCases/listCategories/index.ts
@@ -2,7 +2,7 @@ import { CategoriesRepository } from "../../repositories/CategoriesRepository";
 import { ListCategoryController } from "./ListCategoriesController";
 import { ListCategoryUseCase } from "./ListCategoriesUseCase";
 
-const categoryRepository = new CategoriesRepository();
+const categoryRepository = CategoriesRepository.getInstance();
 const listCategoriesUsecase = new ListCategoryUseCase(categoryRepository);
 const listCategoriesController = new ListCategoryController(
     listCategoriesUsecase
