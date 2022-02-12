1ac68e2b21e883c017b862019e2ef8866dfa0a53
Juliano Vendramini Codognos <66084938+JulianoVendramini@users.noreply.github.com>
2022-02-11 21:16:50 -0300

    chore

--- 04-rentalx/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ 04-rentalx/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,17 @@
+import { parse } from "csv-parse";
+import fs from "fs";
+
 class ImportCategoryUseCase {
-    execute(file: any) {
-        console.log(file);
+    execute(file: Express.Multer.File): void {
+        const stream = fs.createReadStream(file.path);
+
+        const parserFile = parse();
+
+        stream.pipe(parserFile);
+
+        parserFile.on("data", async (line) => {
+            console.log(line);
+        });
     }
 }
 
